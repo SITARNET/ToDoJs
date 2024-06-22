e2e77855be82a2bffd437a01629a35d4051b0861
SITARNET <sitarnet@ukr.net>
2024-06-22 18:24:15 +0300

    Add task, delete task in localstorage

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const taskInput = document.querySelector('#taskInput');
 const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
 
+let tasks = [];
+
 // додаємо завдання
 form.addEventListener('submit', addTask);
 
@@ -20,10 +22,22 @@ function addTask (event) {
 
     // достаєм текст завдання із поля вводу
     const taskText = taskInput.value
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        done: false
+    };
+
+    // додаємо завдання в масив з завданнями
+    tasks.push(newTask);
+
+    // формуємо розмітку для нового завдання
+    const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
     
     // генеруємо розмітку для нового завдання
-    const taskHTML = `<li class="list-group-item d-flex justify-content-between task-item">
-                        <span class="task-title">${taskText}</span>
+    const taskHTML = `<li id="${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
+                        <span class="${cssClass}">${newTask.text}</span>
                             <div class="task-item__buttons">
                                 <button type="button" data-action="done" class="btn-action">
                                     <img src="./img/tick.svg" alt="Done" width="18" height="18">
@@ -53,6 +67,29 @@ function deleteTask (event) {
 
     // перевіряємо що клік був по кнопці Видалити завдання
     const parenNode = event.target.closest('.list-group-item');
+
+    // знаходимо ID завдання
+    const id = Number(parenNode.id);
+
+    // знаходимо індекс завдання у масиві
+    // const index = tasks.findIndex(function (task) {
+    //     return task.id === id;
+    // })
+
+    // стрілкова функція
+    //const index = tasks.findIndex((task) => task.id === id);
+
+    // видаляємо завдання з масиву з завданнями
+    //tasks.splice(index, 1);
+
+    // видалення завдання за допомогою сортування
+    //tasks = tasks.filter(function (task) {
+    //    return task.id === id;
+    //})
+
+    // стрілкова функція
+    tasks = tasks.filter((task) => task.id !== id);
+
     parenNode.remove();
 
     // якщо є одне завдання, то показуємо блок Список завдань пустий
